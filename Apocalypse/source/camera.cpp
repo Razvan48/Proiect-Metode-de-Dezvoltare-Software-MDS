b5c00855d181df861ce684ee828ba1422cb0a90a
sebi <sebimih@gmail.com>
2024-03-04 22:53:39 +0200

    camera + map

---
+++ Apocalypse/source/camera.cpp
@@ -0,0 +1,24 @@
+#include "camera.h"
+
+Camera& Camera::get()
+{
+	static Camera instance;
+
+	return instance;
+}
+
+void Camera::setX(double x)
+{
+	this->x = x;
+}
+
+void Camera::setY(double y)
+{
+	this->y = y;
+}
+
+void Camera::initializeCoords(double x, double y)
+{
+	this->setX(x);
+	this->setY(y);
+}
