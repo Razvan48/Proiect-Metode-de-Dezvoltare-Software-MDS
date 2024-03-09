c5b97bc6087415cd2f7b26e7cf3dd70a61c98781
Răzvan Nicolae Căpățînă <razvannicolae48@gmail.com>
2024-03-09 22:47:47 +0200

    .

--- Apocalypse/source/main.cpp
+++ Apocalypse/source/main.cpp
@@ -1,14 +1,17 @@
+/*
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 #include <glm/glm.hpp>
+*/
 
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
+//#define STB_IMAGE_IMPLEMENTATION
+//#include "stb_image.h"
 
+#include "windowManager/windowManager.h"
 #include <iostream>
 
-void processInput(GLFWwindow* window);
+//void processInput(GLFWwindow* window);
 
 // window settings
 const unsigned int SCR_WIDTH = 1200;
@@ -16,43 +19,8 @@ const unsigned int SCR_HEIGHT = 900;
 
 int main()
 {
-	std::cout << "Hello World!\n";
 
-	// Init GLFW
-	glfwInit();
-
-	// Set all the required options for GLFW
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE); // TODO: GL_TRUE?
-
-	// Create a GLFWwindow object that we can use for GLFW's functions
-	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Apocalypse", NULL, NULL);
-	if (window == nullptr)
-	{
-		std::cout << "Failed to create GLFW window" << std::endl;
-		glfwTerminate();
-		return -1;
-	}
-
-	// Set GLFW context
-	glfwMakeContextCurrent(window);
-
-	// Set the required callback functions
-	// TODO
-	// glfwSetKeyCallback(window, key_callback);
-	// glfwSetMouseButtonCallback(window, mouse_button_callback);
-
-	// Load OpenGL functions, gladLoadGL returns the loaded version, 0 on error
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-	{
-		std::cout << "Failed to initialize OpenGL context" << std::endl;
-		return -1;
-	}
-
-	// Successfully loaded OpenGL
-	std::cout << "Loaded OpenGL " << std::endl;
+	/*
 
 	///////////////////////////////////////////////////
 
@@ -89,9 +57,12 @@ int main()
 
 	// Terminates GLFW, clearing any resources allocated by GLFW
 	glfwTerminate();
+
+	*/
 	return 0;
 }
 
+/*
 void processInput(GLFWwindow* window)
 {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
@@ -99,4 +70,5 @@ void processInput(GLFWwindow* window)
 		glfwSetWindowShouldClose(window, true);
 	}
 }
+*/
 
--- Apocalypse/source/windowManager/windowManager.h
+++ Apocalypse/source/windowManager/windowManager.h
@@ -7,6 +7,7 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+#define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
 #include <iostream>
