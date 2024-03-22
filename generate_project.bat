549948928bbca35ffab1882a087fbe6718f525e8
sebi <sebimih@gmail.com>
2024-03-22 14:12:54 +0200

    post-merge hook

--- generate_project.bat
+++ generate_project.bat
@@ -1,11 +1,17 @@
 @echo off
 setlocal
 
+REM install post-checkout hook
 set "source=.githooks\post-checkout"
 set "destination=.git\hooks"
-
 copy /y "%source%" "%destination%"
 
+REM install post-merge hook
+set "source=.githooks\post-merge"
+set "destination=.git\hooks"
+copy /y "%source%" "%destination%"
+
+REM run premake
 call vendor\premake5\premake5.exe vs2022
 
 PAUSE
