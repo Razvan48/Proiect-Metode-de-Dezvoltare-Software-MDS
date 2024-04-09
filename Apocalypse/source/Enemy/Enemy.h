e60b80fb34faa5178b41d29041cf60db99076f5f
Răzvan Nicolae Căpățînă <razvannicolae48@gmail.com>
2024-04-09 23:51:41 +0300

    added enemy class and minor changes

---
+++ Apocalypse/source/Enemy/Enemy.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "../Human/Human.h"
+#include "../Entity/AIEntity.h"
+
+class Enemy : public virtual Human, public virtual AIEntity
+{
+protected:
+
+
+public:
+
+	Enemy(double x, double y, double drawWidth, double drawHeight, double rotateAngle, double speed, double collideWidth, double collideHeight, const std::vector<std::string>& animationsName2D, double health, double stamina, double armor);
+	~Enemy();
+};
--- Apocalypse/source/Player/Player.cpp
+++ Apocalypse/source/Player/Player.cpp
@@ -1,7 +1,7 @@
 #include "Player.h"
 
-Player::Player(double x, double y, double drawWidth, double drawHeight, double rotateAngle, double speed, double collideWidth, double collideHeight, const std::vector<std::string>& animationsName2D, double health, double armor) :
-	Human(x, y, drawWidth, drawHeight, rotateAngle, speed, collideWidth, collideHeight, animationsName2D, health, armor)
+Player::Player(double x, double y, double drawWidth, double drawHeight, double rotateAngle, double speed, double collideWidth, double collideHeight, const std::vector<std::string>& animationsName2D, double health = 100.0, double stamina = 100.0, double armor = 0.0) :
+	Human(x, y, drawWidth, drawHeight, rotateAngle, speed, collideWidth, collideHeight, animationsName2D, health, stamina, armor)
 {
 
 }
